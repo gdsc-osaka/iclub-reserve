b2f62de4110da7864889fd8de186f59045800bf6
Kariyan126 <kariyan256x@gmail.com>
2026-06-18 18:17:16 +0900

    Reservation型のinterfaceを再定義

--- app/types/reservation.ts
+++ app/types/reservation.ts
@@ -1,3 +1,9 @@
 interface Reservation {
-    
+    id:number,
+    name:string,
+    description:string,
+    photo_url:string,
+    google_calendar_id:string,
+    calendar_url:string,
+    is_active:boolean
 }
