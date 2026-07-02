6996355a9873ce8bc1744180f5b6951b33bc2889
NAKANISHI SHUJI <nknsshuji@gmail.com>
2026-07-02 16:36:50 +0900

    メンバーシップのDBを追加

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -27,5 +27,35 @@ export const groupsTable = sqliteTable("groups", {
     .$defaultFn(() => new Date()),
 });
 
+export enum MembershipRole {
+  Owner = "owner",
+  Member ="member"
+}
+export const membershipTable = sqliteTable("membership", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => createId()),
+
+  name: text("id", { length: 100 }).notNull(),
+
+  userId: text("user_id").references(()=> usersTable.id)
+    .notNull(),
+
+  groupId: text("group_id").references(()=> groupsTable.id)
+    .notNull(),
+
+  role: text("role")
+   .$type <MembershipRole>()
+   .notNull(),
+   
+  createdAt: integer("created_at", { mode: "timestamp_ms" })
+    .notNull()
+    .$defaultFn(() => new Date()),
+  updatedAt: integer("updated_at", { mode: "timestamp_ms" })
+    .notNull()
+    .$defaultFn(() => new Date()),
+});
+
+
 export type Group = typeof groupsTable.$inferSelect;
 export type NewGroup = typeof groupsTable.$inferInsert;
